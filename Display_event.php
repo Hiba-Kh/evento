cd4e08e494d5075d4ada3be8add3f7ed0d225cbd
= <=>
2017-11-06 19:11:20 +0200

    11-6-2017

---
+++ Display_event.php
@@ -0,0 +1,76 @@
+<?php
+header('Content-Type: application/json');
+$servername = "localhost";
+$database = "evento";
+$username = "root";
+$password = "";
+$id = $_GET["id"];
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+class Anything {
+    var $id;
+    var $location;
+    var $name_event;
+    var $start;
+    var $start_date;   
+    var $end_date;
+    var $end;
+}
+$event_id;
+$mysql_qry="SELECT * FROM event_type where event_type_name='$id' ";
+$r=mysqli_query($conn,$mysql_qry);
+$row= mysqli_fetch_assoc($r);
+$event_type_id = $row['event_type_id']; 
+$mysql_qry2="SELECT * FROM my_event where event_type_id='$event_type_id'";
+$r2=mysqli_query($conn,$mysql_qry2);
+
+$i=0;
+$j=0;
+$l=0;
+$arr=array();
+$arr_date=array();
+$arr_date_sorted=array();
+if (!$r2)
+{
+       echo "NO events for that Category";
+   die(mysqli_error($conn)); 
+}
+else {
+  
+while ($row2= mysqli_fetch_assoc($r2)) 
+        {
+$myObj0 = new Anything();
+$myObj0->id = $row2['event_id'];
+$myObj0->location =$row2['location_id'];
+$myObj0->name_event = $row2['event_name'];
+$myObj0->start = $row2['start_time'];
+$myObj0->start_date = $row2['start_date'];
+$myObj0->end_date = $row2['end_date'];
+$myObj0->end = $row2['end_time'];
+
+$arr_date[$j] =  $row2['start_date'];
+
+$arr[$i]=$myObj0;
+$i++;
+$j++;
+    }
+}
+function date_sort($a, $b) {
+    return strtotime($a) - strtotime($b);
+}
+usort($arr_date, "date_sort");
+
+for ($k=0,$size=count($arr); $k < $size;$k++){
+  for ($h=0,$sizeH=count($arr); $h < $sizeH;$h++){
+   if ($arr[$h]->start_date == $arr_date[$k])
+   {
+       $arr_date_sorted[$k]=$arr[$h];
+       break;
+   }
+        }
+}
+echo json_encode($arr_date_sorted);
+header("Location:Display_event.html");
+
+?>
+
---
+++ category.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+    
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="assets/css/style.css">
+      <style>
+*{margin:0px; padding:0px; font-family:Helvetica, Arial, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 90%;
+    padding: 12px 20px;
+    margin: 8px 26px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+	font-size:16px;
+}
+
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 26px;
+    border: none;
+    cursor: pointer;
+    width: 90%;
+	font-size:20px;
+}
+button:hover {
+    opacity: 0.8;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+.avatar {
+    width: 200px;
+	height:200px;
+    border-radius: 50%;
+}
+
+/* The Modal (background) */
+.modal {
+	display:none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.4);
+}
+
+/* Modal Content Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 4% auto 15% auto;
+    border: 1px solid #888;
+    width: 40%; 
+	padding-bottom: 30px;
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+.close:hover,.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    animation: zoom 0.6s
+}
+@keyframes zoom {
+    from {transform: scale(0)} 
+    to {transform: scale(1)}
+}
+</style>
+</style>
+<body background="../background1.png">
+
+  <?php
+    require "conn.php";
+    require "models.php";
+   
+    if (isset($_POST['taskOption'])){
+    $selectOption = $_POST['taskOption'];
+    
+    header("Location:Display_event.php?id=$selectOption");}
+
+?>
+
+    <form class="modal-content animate"  action="category.php" method="post">
+        
+    <div class="imgcontainer">
+      <h1 style="text-align:center;color: green;">Please choose the category of the conferences you are interested in to see</h1>
+    </div>
+
+    <div class="container">
+      <div class="row">
+       <div class="form-group input-group">
+   <select class="form-control" name="taskOption" id="category"  required>
+                                <option readonly>Choose Category</option> 
+                                <option>All</option>
+                                <option>Medicine</option>
+                                <option>Politics</option>
+                                <option>Sport</option>
+                                <option>Technology</option>
+                                <option>Art</option>
+                                <option>Engineering</option>
+                                <option>Science</option>
+                                <option>Food</option>
+                                <option>Law</option>
+                                <option>Economy</option>                                
+                                <option>Media and TV </option>                           
+                              
+        </select>
+        </div>
+      </div> 
+        <input type="submit" value="Press Ok" style="color: green"/>
+     
+    </div>
+    
+  </form>
+  
+</body>
+</html>
+
+
+
+
+
+ 
