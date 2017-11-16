77586b310f6d0752f950b0c0f558ebac52edb6db
= <=>
2017-11-16 14:01:25 +0200

    16-11-2017/2:00PM

--- x.php
+++ x.php
@@ -38,26 +38,24 @@ while ($row= mysqli_fetch_assoc($r))
         {
 $myObj0 = new Anything();
 $myObj0->id = $row['event_id'];
+$id=$row['event_id'];
 $myObj0->location =$row['location_id'];
 $myObj0->name_event = $row['event_name'];
 $myObj0->start_date = $row['start_date'];
 $myObj0->end_date = $row['end_date'];
-
 $arr_date[$j] =  $row['start_date'];
+
 $mysql_qry3="SELECT * FROM agenda WHERE agenda.event_id = $id ";
 $r3=mysqli_query($conn,$mysql_qry3);
-
-if (!r3)
+if (!$r3)
 {
- $myObj0->start ='UNKNOWN';
-$myObj0->end = 'UNKNOWN'; 
+$myObj0->start = 'un';
+$myObj0->end ='un';
 }
 else{
  $row3= mysqli_fetch_assoc($r3);
 $myObj0->start = $row3['start_time'];
 $myObj0->end = $row3['end_time'];
-        
-
 }
 $arr[$i]=$myObj0;
 $i++;
